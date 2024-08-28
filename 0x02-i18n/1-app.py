ae720b9a637196be348ff61016a21468dba0ec05
thanks299 <thanksagbeble@gmail.com>
2024-08-28 13:05:51 -0700

    0x02-i18n

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""A basic flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """A home page 
+    """ 
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0' port=5000)
