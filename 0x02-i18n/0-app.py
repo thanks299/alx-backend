99d0d6d5a5fcf505cd2c09ca2bbe03e5a29e215c
thanks299 <139892876+thanks299@users.noreply.github.com>
2024-08-29 19:32:24 +0100

    0x02i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
@@ -15,3 +16,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' port=5000)
+    
