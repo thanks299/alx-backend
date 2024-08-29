30e61d8277586267618dedcba9d5c8b7b8fc18a9
thanks299 <thanksagbeble@gmail.com>
2024-08-29 15:47:00 -0700

    0x02-i18n

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -19,7 +19,6 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@babel.localeselector
 def get_locale() -> str:
     """Retrieves the locale for a web page.
     """
@@ -33,6 +32,7 @@ def get_locale() -> str:
             return query_table['locale']
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
+babel.locale_selector_func = get_locale
 
 @app.route('/')
 def get_index() -> str:
