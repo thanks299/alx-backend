cf7c094284def16b1512117c47639f5aae3c0a0b
thanks299 <139892876+thanks299@users.noreply.github.com>
2024-08-29 22:51:06 +0100

    0x02i18n

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -19,12 +19,12 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@babel.localeselector
 def get_locale() -> str:
     """Retrieves the locale for a web page.
     """
     return request.accept_languages.best_match(app.config["LANGUAGES"] = ['en' 'fr'])
 
+babel.locale_selector_func = get_locale
 
 @app.route('/')
 def get_index() -> str:
