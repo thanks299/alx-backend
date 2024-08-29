127a79d608078f969231c94536f783941207a740
thanks299 <139892876+thanks299@users.noreply.github.com>
2024-08-30 00:43:55 +0100

    0x02i18n

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -30,6 +30,8 @@ def get_locale() -> str:
 def get_index() -> str:
     """The home/index page.
     """
+    # Pass a message as a parameter to the template
+    greeting = _("Hello, world")
     return render_template('3-index.html')
 
 
