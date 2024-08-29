ca3602f3ecb05bc652c419d1e655391fc50e38a5
thanks299 <thanksagbeble@gmail.com>
2024-08-29 11:26:20 -0700

    x02-i18

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config:
     """Represents a Flask Babel configuration.
     """
@@ -15,11 +16,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @app.route('/')
 def index():
     """A home page 
     """ 
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' port=5000)
